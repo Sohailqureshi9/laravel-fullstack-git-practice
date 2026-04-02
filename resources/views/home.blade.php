2ecfd36113979eba9cf5677dba821ae133f9efe7
Sohail <sohailqureshii369@gmail.com>
2026-04-02 12:57:06 +0500

    testing DevA

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,7 +32,7 @@
             </p>
 
             <div class="buttons">
-                <a class="btn btn-primary" href="#commands">Start Learning</a>
+                <a class="btn btn-primary" href="#commands">Start Learnings</a>
                 <a class="btn btn-secondary" href="{{ route('about') }}">About Project</a>
                 <a class="btn btn-secondary" href="{{ route('contact') }}">Contact</a>
             </div>
